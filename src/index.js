cbbc3e6db3b163945c3f358485dfc54ebb20f2f5
NellDora <dorashin9471@gmail.com>
2024-01-26 23:00:45 +0900

    리액트 돔 사용

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Library from './chapter_03/Library'; //라이브러리 jsx 호출 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Library/>
   </React.StrictMode>
 );
 
